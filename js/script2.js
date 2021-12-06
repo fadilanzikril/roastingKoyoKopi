8083c6b05d2c066f2dcdbc6672fe5fbc9b13a618
fadilanzikril <fadilanzikril76@gmail.com>
2021-12-07 01:15:59 +0700

    first commit

---
+++ js/script2.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $('.navbar-nav').on('click', 'a', function () {
+    $('.navbar-nav a.active').removeClass('active');
+    $(this).addClass('active');
+  })
+});
+
+const parentContainer = document.querySelector('.read_container');
+parentContainer.addEventListener('click', event => {
+
+  const current = event.target;
+  const isbacaBtn = current.className.includes('btn_baca');
+  if (!isbacaBtn) return;
+  const currentText = event.target.parentNode.querySelector('.baca_text');
+  currentText.classList.toggle('baca_show');
+  current.textContent = current.textContent.includes('Selengkapnya') ? "Tutup" : "Selengkapnya...";
+})
+
+
