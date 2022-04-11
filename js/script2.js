87dfdc80fc77ecfefe5066fe4bc4b6f15ae347dd
fadilanzikril <fadilanzikril76@gmail.com>
2022-04-11 11:46:18 +0700

    update terbaeu 11 april 2022

--- js/script2.js
+++ js/script2.js
@@ -1,10 +1,13 @@
+// smooth scroll
 $(document).ready(function () {
   $('.navbar-nav').on('click', 'a', function () {
     $('.navbar-nav a.active').removeClass('active');
     $(this).addClass('active');
   })
 });
+// about
 
+// baca selengkapnya
 const parentContainer = document.querySelector('.read_container');
 parentContainer.addEventListener('click', event => {
 
@@ -14,6 +17,8 @@ parentContainer.addEventListener('click', event => {
   const currentText = event.target.parentNode.querySelector('.baca_text');
   currentText.classList.toggle('baca_show');
   current.textContent = current.textContent.includes('Selengkapnya') ? "Tutup" : "Selengkapnya...";
-})
+});
+
+
 
 
